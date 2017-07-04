b3b133677d50cca9804b0bb3f3423009e4c1d6ef
han0ah <han0ah92@gmail.com>
2017-07-04 15:29:20 +0900

    1) BaselineDisambiguater에서 정의문의 없는 것은 무시하도록 함. 2) disambiguater에 RandomdisAmbiguater 추가

--- disambiguater.py
+++ disambiguater.py
@@ -1,5 +1,6 @@
 import math
 import data_util
+import random
 from data_manager import DataManager
 from sklearn.metrics.pairwise import cosine_similarity
 
@@ -43,6 +44,8 @@ class BaselineDisambiguater(Disambiguater):
         max_word_def = None
 
         for cornet_def in matching_def_list:
+            if (len(cornet_def['definition1']) < 1):
+                continue
             input_text = input['text']
             cornet_def_sent = data_util.convert_def_to_sentence(cornet_def)
             sentences = [input_text, cornet_def_sent]
@@ -63,6 +66,36 @@ class BaselineDisambiguater(Disambiguater):
         '''
         주어진 word의 후보가 될 수 있는 definition list를 가져온다.
         이 Baseline의 경우에는 term이 word랑 완전히 일치할 때만 반환.
+        '''
+        matching_def_list = []
+        for word_def in DataManager.corenet_data:
+            if (word_def['term'] == word):
+                matching_def_list.append(word_def)
+        return matching_def_list
+
+class RandomDisambiguater(Disambiguater):
+    def disambiguate(self, input):
+        if not DataManager.isInitialized:
+            return []
+        matching_def_list = self.get_def_candidate_list(input['word'])
+
+        random_value = random.randrange(0,len(matching_def_list)+1)
+
+        if (random_value == len(matching_def_list)):
+            return []
+
+        selected_def = matching_def_list[random_value]
+
+        return [{
+            'lemma': selected_def['term'],
+            'sensid': '(' + str(selected_def['vocnum']) + ',' + str(selected_def['semnum']) + ')',
+            'definition': selected_def['definition1']
+        }]
+
+    def get_def_candidate_list(self, word):
+        '''
+        주어진 word의 후보가 될 수 있는 definition list를 가져온다.
+        이 Disambiguater는 term이 word랑 완전히 일치할 때만 반환.
         '''
         matching_def_list = []
         for word_def in DataManager.corenet_data:
