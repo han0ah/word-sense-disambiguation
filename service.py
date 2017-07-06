a11302638df4385e64e5a1c4ec97e056933133b4
han0ah <han0ah92@gmail.com>
2017-07-06 21:34:34 +0900

    cross-domain 이슈 해결

--- service.py
+++ service.py
@@ -3,10 +3,24 @@ REST API 처리를 담당하는 모듈
 '''
 import json
 from disambiguater_wrapper import DisambiguaterWrapper
-from bottle import Bottle, run, post, request
+from bottle import Bottle, run, post, request, response
 from data_manager import DataManager
 
+
+def enable_cors(fn):
+	def _enable_cors(*args, **kwargs):
+		# set CORS headers
+		response.headers['Access-Control-Allow-Origin'] = '*'
+		response.headers['Access-Control-Allow-Methods'] = 'GET, POST, PUT, OPTIONS'
+		response.headers['Access-Control-Allow-Headers'] = 'Origin, Accept, Content-Type, X-Requested-With, X-CSRF-Token'
+
+		if request.method != 'OPTIONS':
+			return fn(*args, **kwargs)
+
+	return _enable_cors
+
 @post('/disambiguate')
+@enable_cors
 def disambiguate():
     '''
     문장과 문장 내의 특정 단어가 주어졌을 때, 
