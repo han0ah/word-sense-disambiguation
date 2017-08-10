d5b22cb1d2b838642d7089a63c40981cedecb596
han0ah <han0ah92@gmail.com>
2017-08-10 19:16:47 +0900

    kortermnum 까지 측정할 수 있도록 함

--- performance_measure.py
+++ performance_measure.py
@@ -1,7 +1,7 @@
 import disambiguater
 import data_manager
 
-f = open('test_data2','r', encoding='utf-8')
+f = open('test_data3','r', encoding='utf-8')
 
 def extract_disambiguate_obj_from_text(input_text):
     beginIdx = 0
@@ -35,6 +35,10 @@ ok_hard_cnt = 0
 total_soft_cnt = 0
 ok_soft_cnt = 0
 
+
+ok_hard_kortermnum = 0
+ok_soft_kortermnum = 0
+
 idx = 0
 
 for line in f:
@@ -44,8 +48,9 @@ for line in f:
 
     idx += 1
     print(str(idx) + 'item finished')
-    input_text, result, ishard = line.split('\t')
+    input_text, result, ishard, korterms = line.split('\t')
     ishard = ishard.strip()
+    korterms = korterms.strip()
     if (result.startswith('CONTEXT_')):
         continue
 
@@ -59,12 +64,16 @@ for line in f:
 
     m_disambiguater = disambiguater.BaselineDisambiguater()
     system_output = m_disambiguater.disambiguate(input)
+    word = m_disambiguater.input['word']
     if (len(system_output) == 0 and result.startswith('SENSE_')):
         if (ishard == '1'):
             ok_hard_cnt += 1
+            ok_hard_kortermnum += 1
         else:
             ok_soft_cnt += 1
+            ok_soft_kortermnum += 1
         print ('(-1,-1) match!')
+
     if (len(system_output) > 0 and system_output[0]['sensid'] == result):
         if (ishard == '1'):
             ok_hard_cnt += 1
@@ -72,7 +81,25 @@ for line in f:
             ok_soft_cnt += 1
         print (system_output[0]['sensid'] + ' ' + result + ' matched!')
 
+    if (len(system_output) > 0 and (not result.startswith('SENSE_')) ):
+        system_kortermnum = system_output[0]['kortermnum']
+        korterm_list = korterms.split(',')
+        is_same = False
+        for korterm in korterm_list:
+            if system_kortermnum == korterm:
+                is_same = True
+                break
+        if (is_same):
+            if (ishard == '1'):
+                ok_hard_kortermnum += 1
+            else:
+                ok_soft_kortermnum += 1
+            print (system_kortermnum + ' matched!')
+
 print ('total_hard : %d / accurate : %d / accuracy : %.3f'%(total_hard_cnt, ok_hard_cnt, (ok_hard_cnt/total_hard_cnt)))
 print ('total_soft : %d / accurate : %d / accuracy : %.3f'%(total_soft_cnt, ok_soft_cnt, (ok_soft_cnt/total_soft_cnt)))
+
+print ('total_hard_korterm : %d / accurate : %d / accuracy : %.3f'%(total_hard_cnt, ok_hard_kortermnum, (ok_hard_kortermnum/total_hard_cnt)))
+print ('total_soft_korterm : %d / accurate : %d / accuracy : %.3f'%(total_soft_cnt, ok_soft_kortermnum, (ok_soft_kortermnum/total_soft_cnt)))
 f.close()
 
