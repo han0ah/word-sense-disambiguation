c170990c672f53863ac69bd77fced16e0fd2fbc0
han0ah <han0ah92@gmail.com>
2017-08-10 16:27:24 +0900

    data manager에서 학습된 tfidf object 불러오도록 함

--- data_manager.py
+++ data_manager.py
@@ -1,3 +1,4 @@
+import pickle
 from sklearn.externals import joblib
 
 class DataManager():
@@ -7,10 +8,14 @@ class DataManager():
     # corenet definition data
     corenet_data = None
 
+    # corenet object data
+    corenet_obj = None
+
     isInitialized = False
 
     @staticmethod
     def init_data():
         DataManager.tfidf_obj = joblib.load('./data/trained_tfidf.pkl')
+        DataManager.corenet_obj = pickle.load(open('./data/corenet_obj.pickle', 'rb'))
         #DataManager.corenet_data = data_util.read_corenet_definition_data()
         DataManager.isInitialized = True
