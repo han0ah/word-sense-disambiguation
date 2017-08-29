1bd4694bac85869a9339df8f82ce7f210312bcf6
han0ah <han0ah92@gmail.com>
2017-08-29 22:11:14 +0900

    업데이트된 data_manager 추가

--- data_manager.py
+++ data_manager.py
@@ -11,11 +11,15 @@ class DataManager():
     # corenet object data
     corenet_obj = None
 
+    # korterm tfidf object
+    korenet_tfidf = None
+
     isInitialized = False
 
     @staticmethod
     def init_data():
         DataManager.tfidf_obj = joblib.load('./data/trained_tfidf_etri_tokenize.pkl')
-        DataManager.corenet_obj = pickle.load(open('./data/corenet_obj.pickle', 'rb'))
+        #DataManager.corenet_obj = pickle.load(open('./data/corenet_obj.pickle', 'rb'))
         #DataManager.corenet_data = data_util.read_corenet_definition_data()
+        DataManager.korenet_tfidf = joblib.load('./data/korterm_tfidf.pickle')
         DataManager.isInitialized = True
