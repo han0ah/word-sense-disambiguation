cd01cad33ede9dc637e5d3f048af4bcdc8e55efb
han0ah <han0ah92@gmail.com>
2017-10-20 01:57:25 +0900

    전달 모듈에서 필요없는 데이터 파일 제거

--- data_manager.py
+++ data_manager.py
@@ -20,6 +20,5 @@ class DataManager():
     def init_data():
         DataManager.tfidf_obj = joblib.load('./data/trained_tfidf_etri_tokenize.pkl')
         DataManager.corenet_obj = pickle.load(open('./data/corenet_lemma_info_obj_new.pickle', 'rb'))
-        #DataManager.corenet_data = data_util.read_corenet_definition_data()
         DataManager.korenet_tfidf = joblib.load('./data/korterm_tfidf.pickle')
         DataManager.isInitialized = True
